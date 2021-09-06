14e9e7227bae0df61d2e7cddd68fc0a8c78e22c9
rootys <jaxent@yandex.ru>
2021-09-06 13:17:14 +0300

    reworkd

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Form from "./component/Form.jsx";
+
+const App = () => {
+  return (<div className="d-flex flex-column h-100"> 
+  <Form />
+    </div>);
+};
+
+export default App;
+
---
+++ src/components/Form.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Form = () => {
+	return (
+      <div className="container-fluid h-100" id='form'></div>
+		)
+};
+
+export default Form;
