18c2277203a5621df19a1704205b3afe2d312895
rootyss <jaxent@yandex.ru>
2021-09-08 14:08:09 +0300

    1 step

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+  useLocation
+} from "react-router-dom";
 import Form from "./components/Form.jsx";
+import NoMatch from "./components/NoMatch.jsx";
 
 const App = () => {
-  return (<div className="d-flex flex-column h-100"> 
-  <Form />
-    </div>);
+  return (
+    <Router>
+        <Switch>
+          <Route exact path="/">
+            <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+          </Route>
+          <Route path="/login">
+            <Form />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/NoMatch.jsx
@@ -0,0 +1,12 @@
+import {
+  useLocation
+} from "react-router-dom";
+import React from 'react';
+
+const NoMatch = () => {
+	return (
+        <a className="navbar-brand" href="/">Hexlet Chat (404)</a>
+  );
+}
+
+export default NoMatch;
