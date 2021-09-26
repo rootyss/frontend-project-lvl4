e8b4e13b82e27755ffff8ee7ff4a73d04360c899
rootyss <jaxent@yandex.ru>
2021-09-26 17:36:02 +0300

    fix

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -63,6 +63,7 @@ const ChatWindow = () => {
         <FormGroup className="d-flex justify-content-between mb-3 px-4">
           <span className="mb-0 fs-5">{t('chat.channels')}</span>
           <Button
+            role="button"
             type="button"
             variant="outline-primary"
             className="py-0 btn-sm"
@@ -98,7 +99,7 @@ const ChatWindow = () => {
                   maxLength={400}
                 />
                 <div className="input-group-append">
-                  <button disabled="" type="submit" className="btn btn-group-vertical">
+                  <button disabled="" type="submit" className="btn btn-group-vertical" role="button">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
                       <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
                     </svg>
